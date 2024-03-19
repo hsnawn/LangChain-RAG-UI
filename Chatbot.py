6ae7f7cefcab3ea8e639f9e78687c094314a23b6
Hassaan Zubair <contact@gbtechs.com>
2024-03-19 22:25:40 +0500

    buf fix

--- Chatbot.py
+++ Chatbot.py
@@ -13,8 +13,7 @@ from langchain.prompts import PromptTemplate
 
 class ShippingAssistant:
     def __init__(self, docx):
-        self.docx = docx
-        for file in os.listdir(self.docx):
+        for file in docx:
             self.loader = Docx2txtLoader(file)
             self.docs = self.loader.load()
             self.text_splitter = RecursiveCharacterTextSplitter(
